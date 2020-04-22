2c4e6af3571bb825d97052c6be96ccedcc44484d
c212 <40395648+c212@users.noreply.github.com>
2020-04-22 13:02:29 -0400

    Step 7
    A step without testing is not a step but ... fine. (See attached readme for details on this "step".)

--- 04-22-2020/Graph.java
+++ 04-22-2020/Graph.java
@@ -8,4 +8,44 @@ public class Graph extends HashMap<Node, Neighbors> {
   public Neighbors neighborsOf(Node n) {
     return this.get(n);
   }
+  public Path shortestPath(Node target, Paths candidates) {
+    if (candidates == null) return null; // if there is no starting node there is no path
+    else if (candidates.size() == 0) return new Path(); // if starting node is target node then path is empty
+    else {
+      for (Path candidate : candidates) {
+        //ask if last node in candididate path is target node
+        if (candidate.get(candidate.size() - 1).equals(target)) {
+          //if it is, return that path and you are done!
+          return candidate;}
+      }
+      //if it is not, we need to make a new set of candidates
+      Paths newSet = new Paths();
+      //loop thru each candidate
+      for (Path candidate: candidates){
+        //check to see if path is a dead end
+        if (neighborsOf(candidate.get(candidate.size()-1)) == null){
+          System.out.println(candidate.get(candidate.size()-1) +"is a dead end.");
+        } else {
+          // loop thru the neighbors of the last node of the candidate
+          for (Node n: neighborsOf( candidate.get(candidate.size() -1) )) {
+            //clone the path so we can alter it without altering original object
+            Path clone = candidate.clone();
+            //if the candidate path already contains the node we are looking at that 
+            //means it is not the shortest path and it contains a cycle, so we do not
+            //add it to the new candidate set
+            if (clone.contains(n)) {
+              //do nothing
+            } else {
+              //otherwise add the node we are looking at to the end of the clone path and
+              //add that path as a candidate for the next round
+              clone.add(n);
+              newSet.add(clone);
+          }
+        }
+    } 
+  }
+      //recursively look through each set of candidates until we have a path that reaches the target node
+      return shortestPath(target, newSet);
+  }  
+  }
 }
