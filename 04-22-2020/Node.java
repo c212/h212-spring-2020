249f9bd3a66a26e2cc8d7105283077a972756128
c212 <40395648+c212@users.noreply.github.com>
2020-04-22 12:12:13 -0400

    Step 2

--- 04-22-2020/Node.java
+++ 04-22-2020/Node.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Node {
   String name;
   public Node(String name) {
@@ -5,5 +6,29 @@ public class Node {
   }
   public String toString() {
     return this.name;
+  }
+  public int hashCode() {
+    return this.name.hashCode();
+  }
+  public boolean equals(Object other) {
+    if (other instanceof Node)
+      return this.name.equals(((Node)other).name);
+    else
+      return false;
+  }
+  public static void main(String[] args) {
+    Map<Node, String> a = new HashMap<Node, String>(); //making a new hashmap from nodes
+    //to strings
+    a.put(new Node("A"), "something"); //making a node A mapped to "something"
+    a.put(new Node("A"), "something else"); //this should remap A to "something else" since
+    //hashcode is based on name, not object
+    System.out.println( a ); //should print "{A = something else}", there is only one mapping
+    Node b = new Node("A"); //creating two more nodes with the same name
+    Node c = new Node("A"); 
+    System.out.println(b.hashCode()); //should print the same two hashcodes
+    System.out.println(c.hashCode()); 
+    System.out.println(b.equals(c)); //should be true
+    System.out.println(a.get(b)); //should get "something else" both times
+    System.out.println(a.get(c)); 
   }
 }
